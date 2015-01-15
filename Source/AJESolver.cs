212db0baf52ce7246bc72040e1f464db0a68e11d
blowfishpro <blowfishpro@yahoo.com>
2015-01-15 02:40:19 -0800

    Redundant code
    These two lines do the exact same thing as the one above them.

--- Source/AJESolver.cs
+++ Source/AJESolver.cs
@@ -247,8 +247,6 @@ namespace AJE
                 double snpr = P2 / p0;
                 double ues = Math.Sqrt(2.0 * R_c / fac1 * T2 * eta_n * (1.0 - Math.Pow(1.0 / snpr, fac1)));
                 double pfexit = (snpr <= 1.893) ? p0 : .52828 * P2; //exit pressure of fan 
-                if (snpr <= 1.893) pfexit = p0;
-                else pfexit = .52828 * P2;
                 thrust += BPR * ues * mdot + (pfexit - p0) * BPR * Aref;
             }
 
