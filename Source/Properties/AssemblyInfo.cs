6b007f46a0218c09992bc4203537c8eb2e08c2b5
Alex Wang <aw1621107@gmail.com>
2018-03-19 21:43:33 -0400

    Update for KSP 1.4.1

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: KSPAssembly("AJE", 2, 10)]
+[assembly: KSPAssembly("AJE", 2, 11)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.10.0.0")]
-[assembly: AssemblyFileVersion("2.10.0.0")]
+[assembly: AssemblyVersion("2.11.0.0")]
+[assembly: AssemblyFileVersion("2.11.0.0")]
 
-[assembly: KSPAssemblyDependency("SolverEngines", 3, 3)]
+[assembly: KSPAssemblyDependency("SolverEngines", 3, 6)]
