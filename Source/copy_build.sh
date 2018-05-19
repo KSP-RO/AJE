f3662c0a94086b6e278813d28a0c233ebf324dcc
blowfish <joeydwong@gmail.com>
2018-05-19 14:26:09 -0700

    Post build works if PDB2MDB found but no pdb
    pdb2mdb would fail if it was run but no pdb file present

--- Source/copy_build.sh
+++ Source/copy_build.sh
@@ -6,8 +6,23 @@ elif ! [ -f "${TARGET_PATH}" ] ; then
   exit 1
 fi
 
+if [ -z "${TARGET_DIR}" ] ; then
+  echo 'Expected $TARGET_DIR to be defined but it is not' >&2
+  exit 1
+elif ! [ -d "${TARGET_DIR}" ] ; then
+  echo 'Expected $TARGET_DIR to be a directory but it is not' >&2
+  exit 1
+fi
+
+if [ -z "${TARGET_NAME}" ] ; then
+  echo 'Expected $TARGET_NAME to be defined but it is not' >&2
+  exit 1
+fi
+
 if [ -z "${PDB2MDB}" ] ; then
   echo '$PDB2MDB not found'
+elif ! [ -f "${TARGET_DIR}/${TARGET_NAME}.pdb" ]; then
+  echo "${TARGET_NAME}.pdb not found"
 else
   echo "Running '${PDB2MDB}'"
   "${PDB2MDB}" "${TARGET_PATH}"
