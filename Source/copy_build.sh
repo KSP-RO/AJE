566e168241acd223f01ee6dbfcd9fac1a5bf720c
NathanKell <nathankell@gmail.com>
2021-05-29 20:38:13 -0700

    Let the shell script work on my setup too (should have no ill effects)

--- Source/copy_build.sh
+++ Source/copy_build.sh
@@ -1,7 +1,9 @@
 if [ -z "${TARGET_PATH}" ] ; then
   echo 'Expected $TARGET_PATH to be defined but it is not' >&2
   exit 1
-elif ! [ -f "${TARGET_PATH}" ] ; then
+fi
+
+if ! [ -f "${TARGET_PATH}" ] ; then
   echo 'Expected $TARGET_PATH to be a file but it is not' >&2
   exit 1
 fi
@@ -9,7 +11,9 @@ fi
 if [ -z "${TARGET_DIR}" ] ; then
   echo 'Expected $TARGET_DIR to be defined but it is not' >&2
   exit 1
-elif ! [ -d "${TARGET_DIR}" ] ; then
+fi
+
+if ! [ -d "${TARGET_DIR}" ] ; then
   echo 'Expected $TARGET_DIR to be a directory but it is not' >&2
   exit 1
 fi
@@ -21,9 +25,12 @@ fi
 
 if [ -z "${PDB2MDB}" ] ; then
   echo '$PDB2MDB not found'
-elif ! [ -f "${TARGET_DIR}/${TARGET_NAME}.pdb" ]; then
-  echo "${TARGET_NAME}.pdb not found"
 else
-  echo "Running '${PDB2MDB}'"
-  "${PDB2MDB}" "${TARGET_PATH}"
+  if ! [ -f "${TARGET_DIR}/${TARGET_NAME}.pdb" ]; then
+    echo "${TARGET_NAME}.pdb not found"
+  else
+    echo "Running '${PDB2MDB}'"
+    "${PDB2MDB}" "${TARGET_PATH}"
+  fi
 fi
+exit 0
